271719d12d7c60394c833463fc015e0944e03c8c
praveenkumar006 <32816475+praveenkumar006@users.noreply.github.com>
2018-07-18 13:25:28 +0530

    Add files via upload

---
+++ src/category/category.component.ts
@@ -0,0 +1,19 @@
+import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute, Router } from '../../../node_modules/@angular/router';
+
+@Component({
+  selector: 'app-category',
+  templateUrl: './category.component.html',
+  styleUrls: ['./category.component.css']
+})
+export class CategoryComponent implements OnInit {
+
+  constructor(private route:ActivatedRoute,private router:Router) { }
+
+  ngOnInit() {
+    console.log(this.route.snapshot.params["cat"]);
+    this.router.navigate(['']);
+  }
+
+
+}
